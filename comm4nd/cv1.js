24959b0a9805eb1793c69532aab010d35cdbff0d
h0ndaaa <89423066+h0ndaaa@users.noreply.github.com>
2021-08-26 15:15:26 +0100

    Add files via upload

---
+++ comm4nd/cv1.js
@@ -0,0 +1,33 @@
+exports.wpp = (pushname) => {
+   return `
+「 *Uzi Bot* 」
+
+「 Hello ${pushname}
+  ❏ Prefixo: ${prefix} 」
+
+「 MENUS 」
+  ❏ ${prefix}about
+   ➰ Para mais informações
+  ❏ ${prefix}flix
+   ➰ Sugestões de filmes e séries de todas as categorias
+  ❏ ${prefix}biblioteca
+   ➰ Menu de livros em arquivo .pdf
+  ❏ ${prefix}adms
+   ➰ Menu para ADM
+  ❏ ${prefix}maker
+   ➰ Comandos de imagem com texto
+  ❏ ${prefix}music
+   ➰ Menu de música
+  ❏ ${prefix}fga
+   ➰ Menu de stickers
+  ❏ ${prefix}entretenimento
+   ➰ Meio óbvio XD
+  ❏ ${prefix}consultas
+   ➰ Menu de dados fake
+  ❏ ${prefix}credits
+   ➰ Pessoas que me ajudaram
+
+「 ${pushname} Se precisar de suporte:
+  ❏ Número do criador: wa.me/+351927438657 」`
+`
+}
